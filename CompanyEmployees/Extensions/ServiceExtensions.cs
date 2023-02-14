b929b1230684f3f14fed7df460516e828f438c56
Mohamed Fayez <mohamedfayez664@gmail.com>
2023-02-14 08:50:21 +0200

    Add project files.

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Contracts;
+using LoggerService;
+using Microsoft.AspNetCore.HttpOverrides;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) 
+            => services.AddCors  (options =>
+         {
+        options.AddPolicy  ("CorsPolicy", builder =>
+        builder.AllowAnyOrigin()  // o.WithOrigin("url:port//.com","  ",...);
+        .AllowAnyMethod()  //WithMethods("POST", "GET") 
+        .AllowAnyHeader());  // WithHeaders("accept", "contenttype")
+         });
+
+
+
+
+        /// IIS
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+        services.Configure<IISOptions>(options =>
+        {
+            
+        });
+
+
+
+        ///logger
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+          services.AddSingleton<ILoggerManager, LoggerManager>();
+
+
+    }
+}
+
+
---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,75 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Http;
+using NLog;
+using NLog.Fluent;
+
+
+var builder = WebApplication.CreateBuilder(args); //WebApplicationBuilder
+
+// Add services to the container.(ConfigureServices )
+//
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+"/nlog.config"));
+
+//var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+//LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+//$"/nlog.{environment}.config "));
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+////
+
+builder.Services.AddControllers();
+
+var app = builder.Build();   ////WebApplication>> (Configure)
+                             //IHost, IApplicationBuilder, IEndpointRouteBuilder
+// Configure the HTTP request pipeline.  //middleware
+///
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+/*
+adds a header Strict-Transport-Security to the response. When the site was accessed using HTTPS then 
+the browser notes it down and future request using HTTP will be redirected to HTTPS. So, accessing the 
+site using HTTPS at least once is mandatory to make this work.Also the expiration time set by the 
+Strict-Transport-Security header elapses, the next attempt to load the site via HTTP won't be automatically redirected to HTTPS.
+ */
+
+/////
+app.UseHttpsRedirection();  //redirection from HTTP to HTTPS
+
+/////
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All  //forward proxy headers to the current request
+});
+app.UseCors("CorsPolicy");
+//////
+app.UseAuthorization();
+
+//app.MapControllers();  //to>>  IEndpointRouteBuilder
+app.MapControllers();  //to>>  IEndpointRouteBuilder
+
+
+//app.Use(async (context, next) =>
+//{
+//    Console.WriteLine($"Logic before executing the next delegate in the Use method");
+//    await next.Invoke();
+//    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+//});
+
+
+app.Run();
+//app.Run(async context =>
+//{
+//    Console.WriteLine($"Writing the response to the client in the Run method");
+//    await context.Response.WriteAsync("Hello from the middleware component.");
+//});
+
+
+//app.Run();
